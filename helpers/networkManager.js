691b218fb8542c56300fb6ffc9e5ead3f58688e0
mbdjj <mbdjj@o2.pl>
2026-03-03 13:20:38 +0100

    :bricks: Change API to exchangerate-api.com

--- helpers/networkManager.js
+++ helpers/networkManager.js
@@ -1,9 +1,9 @@
 import { currency } from "./currency"
 
 export async function getData(baseCode, foreignCode) {
-  const url = `/api/currency-data?baseCode=${baseCode}&foreignCode=${foreignCode}`
+  const url = `/api/currency-data?baseCode=${baseCode}`
   const response = await fetch(url)
   const data = await response.json()
 
-  return currency(foreignCode, data.data.data[foreignCode])
+  return currency(foreignCode, data.rates[foreignCode])
 }
